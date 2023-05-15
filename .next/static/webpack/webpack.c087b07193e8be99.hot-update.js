79d218ecd6651997366e4597238de2b90bab14f0
jeffgat <jeff.gtbn@gmail.com>
2023-05-14 21:29:20 -0600

    updated config

--- .next/static/webpack/webpack.c087b07193e8be99.hot-update.js
+++ .next/static/webpack/webpack.c087b07193e8be99.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "e47fe6c70f7fab95"; }
+/******/ 	__webpack_require__.h = function() { return "f39dde6fb63fb6af"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
